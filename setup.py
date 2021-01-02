f6c5d635b392caba26da1d1ee9e8f6a0003cbbda
Yaroslav Khnykov <khnykov@outlook.com>
2021-01-02 17:51:00 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autoru_parser',
+    version='0.0.1',
+    packages=find_packages(),
+    url='#',
+    license='MIT',
+    author='Yaroslav Khnykov',
+    description='Package to collect data from auto.ru'
+)
